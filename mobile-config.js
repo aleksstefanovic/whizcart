b81586f32b7bb2d093f97cbf59997a2fd6aeb624
Aleks <aleks.stefanovic101@gmail.com>
2017-02-26 03:54:51 -0500

    android ready to go

---
+++ mobile-config.js
@@ -0,0 +1,24 @@
+App.info({
+  id: 'com.meteor.app.whizcart',
+  name: 'WhizCart',
+  version: "0.0.1"
+});
+
+App.icons({
+  'android_mdpi': './icons/whizcarticon.png',
+  'android_hdpi': './icons/whizcarticon.png',
+  'android_xhdpi': './icons/whizcarticon.png',
+  'android_xxhdpi': './icons/whizcarticon.png',
+  'android_xxxhdpi': './icons/whizcarticon.png',
+});
+
+App.launchScreens({
+  'android_mdpi_portrait': './icons/whizcartsplash.png',
+  'android_mdpi_landscape': './icons/whizcartsplash.png',
+  'android_hdpi_portrait': './icons/whizcartsplash.png',
+  'android_hdpi_landscape': './icons/whizcartsplash.png',
+  'android_xhdpi_portrait': './icons/whizcartsplash.png',
+  'android_xhdpi_landscape': './icons/whizcartsplash.png',
+  'android_xxhdpi_portrait': './icons/whizcartsplash.png',
+  'android_xxhdpi_landscape': './icons/whizcartsplash.png',
+});
