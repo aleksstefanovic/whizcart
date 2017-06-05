2f65feed2e622100f489d9c60745290e495b7c31
Soren <soren.sabet@gmail.com>
2017-06-04 20:38:37 -0400

    ui more butter

--- mobile-config.js
+++ mobile-config.js
@@ -5,20 +5,20 @@ App.info({
 });
 
 App.icons({
-  'android_mdpi': './icons/whizcarticon.png',
-  'android_hdpi': './icons/whizcarticon.png',
-  'android_xhdpi': './icons/whizcarticon.png',
-  'android_xxhdpi': './icons/whizcarticon.png',
-  'android_xxxhdpi': './icons/whizcarticon.png',
+  'android_mdpi': './public/icons/whizcarticon.png',
+  'android_hdpi': './public/icons/whizcarticon.png',
+  'android_xhdpi': './public/icons/whizcarticon.png',
+  'android_xxhdpi': './public/icons/whizcarticon.png',
+  'android_xxxhdpi': './public/icons/whizcarticon.png',
 });
 
 App.launchScreens({
-  'android_mdpi_portrait': './icons/whizcartsplash.png',
-  'android_mdpi_landscape': './icons/whizcartsplash.png',
-  'android_hdpi_portrait': './icons/whizcartsplash.png',
-  'android_hdpi_landscape': './icons/whizcartsplash.png',
-  'android_xhdpi_portrait': './icons/whizcartsplash.png',
-  'android_xhdpi_landscape': './icons/whizcartsplash.png',
-  'android_xxhdpi_portrait': './icons/whizcartsplash.png',
-  'android_xxhdpi_landscape': './icons/whizcartsplash.png',
+  'android_mdpi_portrait': './public/icons/whizcartsplash.png',
+  'android_mdpi_landscape': './public/icons/whizcartsplash.png',
+  'android_hdpi_portrait': './public/icons/whizcartsplash.png',
+  'android_hdpi_landscape': './public/icons/whizcartsplash.png',
+  'android_xhdpi_portrait': './public/icons/whizcartsplash.png',
+  'android_xhdpi_landscape': './public/icons/whizcartsplash.png',
+  'android_xxhdpi_portrait': './public/icons/whizcartsplash.png',
+  'android_xxhdpi_landscape': './public/icons/whizcartsplash.png',
 });
